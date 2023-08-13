87e29552d5a225b37c067c2d32a15e635bdd40f5
Baurzhan Yussupov <baurzhangulnur@gmail.com>
2023-08-13 13:04:41 +0600

    The ability to enter and send a message to the server via websocket has been added to the 'Chat' container

--- src/containers/ChatWs/ChatWs.js
+++ src/containers/ChatWs/ChatWs.js
@@ -1,10 +1,21 @@
 import React, { useState, useRef, useEffect } from "react";
 
-import { Container, Grid, TextField } from "@mui/material";
+import { Container, Grid, TextField, Button, Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+    flexRowCenter: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+});
 
 const ChatWs = ({ user }) => {
+    const [isLoggedIn, setLoggedIn] = useState(false);
     const [onlineUsers, setOnlineUsers] = useState([]);
-    const [strOnlineUsers, setStrOnlineUsers] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [messageText, setMessageText] = useState("");
 
     const ws = useRef(null);
 
@@ -13,12 +24,14 @@ const ChatWs = ({ user }) => {
             ws.current = new WebSocket(`ws://localhost:8000/chat?token=${user.token}&username=${user.username}`);
             ws.current.onclose = () => {
                 console.log("ws closed");
+                setLoggedIn(false);
             }
 
             ws.current.onopen = () => {
                 ws.current.send(JSON.stringify({
                     type: "GET_ONLINE_USERS"
                 }));
+                setLoggedIn(true);
             }
 
             ws.current.onmessage = event => {
@@ -27,45 +40,126 @@ const ChatWs = ({ user }) => {
                 if (decodedMessage.type === "ONLINE_USERS") {
                     setOnlineUsers(JSON.parse(decodedMessage.users));
                 }
+
+                if (decodedMessage.type === "NEW_MESSAGE") {
+                    setMessages((messages) => [...messages, decodedMessage.message]);
+                }
             };
 
             return () => {
                 if (ws.current.readyState === 1) ws.current.close();
             }
         }
-        else setStrOnlineUsers("");
     }, [user]);
 
     useEffect(() => {
-        let strUsers = "";
+        if (!isLoggedIn) {
+            setOnlineUsers([]);
+            setMessages([]);
+            setMessageText("");
+        }
+    }, [isLoggedIn]);
 
-        onlineUsers.forEach(element => {
-            if (strUsers.length > 0) strUsers += "\n";
-            strUsers += element;
-        });
+    const changeMessage = e => {
+        setMessageText(e.target.value);
+    };
 
-        setStrOnlineUsers(strUsers);
-    }, [onlineUsers]);
+    const sendMessage = () => {
+        ws.current.send(JSON.stringify({
+            type: "CREATE_MESSAGE",
+            username: user.username,
+            text: messageText
+        }));
+
+        setMessageText("");
+    };
+
+    const classes = useStyles();
 
     return (
         <Container component="main" maxWidth="md">
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={4}>
-                    <TextField
-                        label="Online users"
+                    <Box
+                        component="fieldset"
+                        sx={{
+                            height: 380,
+                            overflowY: "scroll",
+                            border: '1px solid lightgray'
+                        }}
+                    >
+                        <legend>Online users</legend>
+                        <ul style={{ padding: "0", margin: "0" }}>{
+                            onlineUsers.map(username =>
+                                <li style={{ listStyle: "none" }} key={username}>{username}</li>
+                            )
+                        }</ul>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                    <Box
+                        component="fieldset"
+                        sx={{
+                            height: 290,
+                            overflowY: "scroll",
+                            border: '1px solid lightgray'
+                        }}
+                    >
+                        <legend>Chat room</legend>
+                        {
+                            messages.map((message, index) =>
+                                <p key={index} style={{ margin: "0" }}><b>{message.username}: </b>{message.text}</p>
+                            )
+                        }
+                    </Box>
+
+                    {/* <Box
+                        sx={{
+                            height: 290,
+                            border: '1px solid lightgray',
+                            '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                            },
+                        }}
+                    >
+                    </Box> */}
+                    {/* <TextField
+                        label="Chat room"
+                        fullWidth
                         multiline
-                        rows={15}
+                        rows={11}
                         InputProps={{
                             readOnly: true,
                         }}
-                        value={strOnlineUsers}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={8}>
-                    sm=8
+                    // value={strOnlineUsers}
+                    /> */}
+                    <Grid container spacing={2} className={classes.flexRowCenter} sx={{ my: 1 }}>
+                        <Grid item xs={12} sm={10}>
+                            <TextField
+                                label="Enter message"
+                                fullWidth
+                                variant="outlined"
+                                InputProps={{
+                                    readOnly: !isLoggedIn
+                                }}
+                                value={messageText}
+                                onChange={changeMessage}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={2}>
+                            <Button
+                                variant="outlined"
+                                disabled={!isLoggedIn}
+                                onClick={sendMessage}
+                            >
+                                Send
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
-        </Container>
+        </Container >
     )
 }
 
